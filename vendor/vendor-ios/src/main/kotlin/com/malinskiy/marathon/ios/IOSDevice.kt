315b5064a87dba48c6ecbd7012c070dc09ea749d
Vitaliy Gorbachov <Vitaliy.Gorbachov@agoda.com>
2022-03-24 00:28:23 +0700

    unzip with measurement

--- vendor/vendor-ios/src/main/kotlin/com/malinskiy/marathon/ios/IOSDevice.kt
+++ vendor/vendor-ios/src/main/kotlin/com/malinskiy/marathon/ios/IOSDevice.kt
@@ -286,10 +286,10 @@ class IOSDevice(val simulator: RemoteSimulator,
                         port = hostCommandExecutor.port
                 )
 
-//                RemoteFileManager.unzipRemoteArchive(this@IOSDevice)
-//                val totalUnzipTime = measureTimeMillis {
-//                }
-//                logger.debug("[RSYNC] Device ${hostCommandExecutor.hostAddress.hostName}:${hostCommandExecutor.port} took $totalUnzipTime to unzip products")
+                val totalUnzipTime = measureTimeMillis {
+                    RemoteFileManager.unzipRemoteArchive(this@IOSDevice)
+                }
+                logger.debug("[RSYNC] Device ${hostCommandExecutor.hostAddress.hostName}:${hostCommandExecutor.port} took $totalUnzipTime to unzip products")
 
                 logger.debug("[TEST-${hostCommandExecutor.hostAddress.hostName}:${hostCommandExecutor.port}] ==== rsync finished ====")
                 this@IOSDevice.derivedDataManager = derivedDataManager
